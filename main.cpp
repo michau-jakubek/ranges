1cb605db750db56cbe25bf41684c4ffa37092da9
Michau Jakubek <michau.jakubek@gmail.com>
2019-05-09 00:46:24 +0200

    Exemplary LINQ implementation in C++

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
+#include <iostream>
+#include <numeric>
 #include "view.hpp"
 
-using os = std::ostream;
-auto& oo = std::cout;
-os& (*nn)(os&) = std::endl;
-
-using namespace view;
+using namespace views;
 
 struct point
 {
@@ -12,43 +10,24 @@ struct point
 	point() : x(640), y(480), z(32) {}
 	point(int x_, int y_) : x(x_), y(y_), z(32) {}
 	point(int x_, int y_, int z_) : x(x_), y(y_) , z(z_) {}
-	friend os& operator<<(os& str, const point& p)
-	{
+	friend std::ostream& operator<<(std::ostream& str, const point& p) {
 		return str << "{ " << p.x << ", " << p.y << ", " << p.z << " }";
 	}
 };
 
-void test_where()
-{
-	std::vector<int> v { 1,2,3,4,5 };
-	auto condition = [](auto&& a){ return 0 == (a % 2); };
-	auto x = v | where(condition) | where([](auto&& a){return a > 3;});
-}
-
-
-void test_selecting()
-{
-	std::vector<int> v { 1,2,3,4,5 };
-	auto x = v | selecting(selector<point>(), splaceholder(), 100);
-	for (auto&& p : x)
-	{
-		oo << p << nn;
-	}
-}
-
 void test_where_and_select()
 {
-	std::vector<int> source(10);
+	std::vector<int> source(20);
 	std::iota(source.begin(), source.end(), 1);
 
 	auto even = [](auto&& a){ return 0 == (a % 2); };
+	auto divisibleBy3 = [](auto& b){ return 0 == (b % 3); };
 	auto pwr2 = [](const int& x) { return x*x; };
 
-	auto v = source | where(even) | selecting(selector<point>(), splaceholder(), splaceholder(pwr2), 256);
-	for (auto&& p : v)
-	{
-		oo << p << nn;
-	}
+	auto v = source | where(even) | where(divisibleBy3)
+		| selecting(selector<point>(), splaceholder(), splaceholder(pwr2), 256);
+
+	std::copy(v.begin(), v.end(), std::ostream_iterator<point>(std::cout, "\n"));
 }
 
 int main()
--- view.hpp
+++ view.hpp
@@ -2,42 +2,76 @@
 #define __VIEW_HPP__
 
 #include <algorithm>
-#include <iostream>
+#include <functional>
 #include <iterator>
-#include <numeric>
-#include <tuple>
-#include <type_traits>
-#include <utility>
 #include <vector>
 #include <viable.hpp>
 
-/**
- * 2019-03-05
- * constexpt where + operator()(lambda)
- * constexpr selecting + operator()(selector<T>, CtorParams...)
- * helper selector<T>
- * helper splaceholder
- */
-
-namespace view
+namespace views
 {
+	// Main view definition.
+	template<class T> using view = std::vector<std::reference_wrapper<T>>;
+
+	// Extracts a value_type from given STL-like collection iterator.
+	template<class I> using value_type_t = typename std::iterator_traits<std::remove_reference_t<I>>::value_type;
+
+	// Generic template reference_unwrapper class.
+	template<class T>
+	struct reference_unwrapper
+	{
+		typedef T type;
+		static T& get(T& x) { return x; }
+	};
+
+	// Specialised template reference_unwrapper class that unwraps
+	// a reference previously wrapped with std::reference_wrapper.
+	template<class T>
+	struct reference_unwrapper<std::reference_wrapper<T>>
+	{
+		typedef T type;
+		static T& get(std::reference_wrapper<T>& x) { return x.get(); }
+	};
+
+	// Makes a view from the iterator range.
+	template<class I, class value_type = value_type_t<I>>
+	auto make_view(I first, I last) -> view<typename reference_unwrapper<value_type>::type>
+	{
+		view<typename reference_unwrapper<value_type>::type> res;
+		std::for_each(first, last, [&res](value_type& x)
+				{
+					res.emplace_back(reference_unwrapper<value_type>::get(x));
+				});
+		return res;
+	}
+
+	// Makes a view from the iterator range but only for which the predicate pred returns true.
+	template<class I, class Pred, class value_type = value_type_t<I>>
+	auto make_view(I first, I last, Pred pred) -> view<typename reference_unwrapper<value_type>::type>
+	{
+		view<typename reference_unwrapper<value_type>::type> res;
+		std::for_each(first, last, [&res, &pred](value_type& x)
+				{
+					if (pred(reference_unwrapper<value_type>::get(x)))
+						res.emplace_back(reference_unwrapper<value_type>::get(x));
+				});
+		return res;
+	}
+
+	// Where clause class definition.
 	struct where_t
 	{
 		template<class I, class F>
 		auto operator()(I&& begin, I&& end, F&& unary_op)
 		{
-			typedef typename
-				std::iterator_traits<std::remove_reference_t<I>>::value_type value_type;
-
-			std::vector<value_type> result;
-
-			std::copy_if(std::forward<I>(begin), std::forward<I>(end),
-				std::back_inserter(result), unary_op);
-
-			return result; 
+			return make_view(begin, end, unary_op);
 		}
 	};
 
+	// Generic splaceholder class definition.
+	// When used as an argument to selecting expression each placeholder is replaced by
+	// currently processed collection element. Additionally placeholder can be parameterized
+	// with callable then underlying callable operator will be called before placeholder is
+	// passed to selecting expression.
 	template<class T>
 	struct splaceholder_t
 	{
@@ -48,16 +82,16 @@ namespace view
 
 	struct splaceholder_default {};
 
+	// Placeholders factory.
 	struct splaceholder_factory
 	{
 		auto operator()() const
 			{ return splaceholder_t<splaceholder_default>(splaceholder_default()); }
+
 		template<class T>
 		splaceholder_t<T> operator()(T&& x) const
 			{ return splaceholder_t<T>(std::forward<T>(x)); }
 	};
-
-	template<class T> struct selector { };
 
 	template<class X>
 	struct fwd
@@ -93,11 +127,16 @@ namespace view
 		template<class Y>
 		auto get(Y&& y) const
 		{
-			static_assert(LI::template from_args<Y>::value, "Unable call lamda with that argument");
+			static_assert(LI::template from_args<Y>::value,
+					"Unable call lamda with that argument");
 			return phdr.get()(y);
 		}
 	};
 
+	// Disambiguation tag for a type used in selecting expression.
+	template<class T> struct selector { };
+
+	// Selecting expression class definition.
 	struct select_t
 	{
 		template<class I, class S, class... Ts>
@@ -107,7 +146,8 @@ namespace view
 				std::iterator_traits<std::remove_reference_t<I>>::value_type value_type;
 
 			static_assert(viable::is_viable<S>::template from_args<
-				decltype(fwd<std::remove_reference_t<Ts>>(std::forward<Ts>(xs)).get(*begin))... >::value, "No matching constructor to call");
+				decltype(fwd<std::remove_reference_t<Ts>>(std::forward<Ts>(xs)).get(*begin))... >::value,
+					"No matching constructor to call");
 
 			std::vector<S> result;
 
@@ -123,6 +163,7 @@ namespace view
 	};
 
 	template<class T> struct functor_wrapper;
+
 	template<class T> auto make_functor_wrapper(T&& f)
 	{
 		return functor_wrapper<T>(std::forward<T>(f));
@@ -132,9 +173,13 @@ namespace view
 	struct functor_wrapper
 	{
 		T functor;
+
 		T get() const { return std::move(functor); }
+
 		constexpr functor_wrapper() = default;
-		functor_wrapper(T&& x) : functor(x) {}
+
+		functor_wrapper(T&& x) : functor(std::move(x)) {}
+
 		template<class F>
 		auto operator()(F&& f) const
 		{
@@ -144,6 +189,7 @@ namespace view
 				return T()(begin, end, f);
 			});
 		}
+
 		template<class S, class... Ts>
 		auto operator()(selector<S>&&, Ts&&... xs) const
 		{
@@ -156,16 +202,16 @@ namespace view
 	};
 
 	template<class T, class F>
-	auto operator|(T&& x, const view::functor_wrapper<F>& f)
+	auto operator|(T&& x, const functor_wrapper<F>& f)
 	{
 		return f.get()(x.begin(), x.end());
 	}
 
-	constexpr functor_wrapper<view::where_t> where{};
-	constexpr functor_wrapper<view::select_t> selecting{};
-	constexpr splaceholder_factory	splaceholder{};
+	constexpr functor_wrapper<where_t>	where{};
+	constexpr functor_wrapper<select_t>	selecting{};
+	constexpr splaceholder_factory		splaceholder{};
 
-} // namespace view
+} // namespace view;
 
 #endif // __VIEW_HPP__
 
